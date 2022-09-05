3d760cf02df7174ada4a98ee44d399577bb8ce8b
JosephGwara <jgwarix@gmail.com>
2022-09-05 22:16:09 +0200

    added method to generate sequencial student id

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Student {
     private String firstName;
     private String surname;
-    public int schoolYear;
-    public int studentId;
+    private int schoolYear;
+    private String studentId;
+    private static int id = 1000;
+    private static int costOfCourse = 600;
     private String courses;
     private String tuitionBalance;
 
@@ -23,10 +25,23 @@ public class Student {
         System.out.println("Enter Student School year:\n1:First year \n2:Second year \n3:Third year \n4:Fourth year");
         this.schoolYear = sc.nextInt();
 
-        System.out.println(firstName+"\n"+surname+"\n"+schoolYear);
+        generateID();
+
+        System.out.println(firstName+"\n"+surname+"\n"+schoolYear+"\n"+studentId);
 
     }
     // Generate Student ID
+    private void generateID(){
+        //increment student id each time a new student is added
+        id++;
+
+        /**this statement adds the student year and id value together
+         *to create the student id the string in between causes the
+         * value to be returned as a string*/
+
+        this.studentId = schoolYear +""+ id;
+
+    }
 
 
 
