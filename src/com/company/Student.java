a59c3c0564b184b9220c45a7c7a3a77fcda61735
JosephGwara <jgwarix@gmail.com>
2022-09-05 21:11:26 +0200

    Created Student Class that gets the sdudents details and stores them

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Student {
+    private String firstName;
+    private String surname;
+    public int schoolYear;
+    public int studentId;
+    private String courses;
+    private String tuitionBalance;
+
+
+    public Student(){
+//Prompt user to enter name ,surname and grade year
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Student Name:");
+        this.firstName = sc.nextLine();
+
+        System.out.println("Enter Student Surname:");
+        this.surname = sc.nextLine();
+
+        System.out.println("Enter Student School year:\n1:First year \n2:Second year \n3:Third year \n4:Fourth year");
+        this.schoolYear = sc.nextInt();
+
+        System.out.println(firstName+"\n"+surname+"\n"+schoolYear);
+
+    }
+    // Generate Student ID
+
+
 
 }
